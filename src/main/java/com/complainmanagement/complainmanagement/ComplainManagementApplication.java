1d6cf26a04aef9e369772a721318c96c7ec6ca74
Dipika Choudhary <dipika.choudhary@oyorooms.com>
2020-03-21 12:53:05 +0530

    initial commit

---
+++ src/main/java/com/complainmanagement/complainmanagement/ComplainManagementApplication.java
@@ -0,0 +1,13 @@
+package com.complainmanagement.complainmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplainManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/complainmanagement/complainmanagement/ComplainManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complainmanagement.complainmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplainManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
